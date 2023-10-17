c9cdcc79e28fee4d1954f8a83946746f96e46d65
p1r473 <subwayjared@gmail.com>
2023-10-17 15:06:21 -0400

    Update WireGuard_Excluded_IPs.py

--- WireGuard_Excluded_IPs.py
+++ WireGuard_Excluded_IPs.py
@@ -4,7 +4,7 @@ import sys
 def parse_ip_networks(ip_list_str):
     ip_list = ip_list_str.split(',')
     networks = []
-    invalid_ips = []  # List to store invalid IPs.
+    invalid_ip_addresses = []  # List to store invalid IPs.
 
     for ip in ip_list:
         ip = ip.strip()
@@ -18,34 +18,53 @@ def parse_ip_networks(ip_list_str):
                 else:
                     networks.append(ipaddress.ip_network(f"{ip}/128", strict=False))
         except ValueError:
-            invalid_ips.append(ip)  # Add invalid IP to the list.
+            invalid_ip_addresses.append(ip)  # Add invalid IP to the list.
 
-    return networks, invalid_ips  # Return both valid networks and invalid IPs.
+    return networks, invalid_ip_addresses  # Return both valid networks and invalid IPs.
 
 def get_input_and_parse(prompt):
     while True:  # Keep looping until we break out.
         user_input = input(prompt)
-        networks, invalid_ips = parse_ip_networks(user_input)  # Get both valid networks and invalid IPs.
+        networks, invalid_ip_addresses = parse_ip_networks(user_input)  # Get both valid networks and invalid IPs.
 
-        if not invalid_ips:  # If there are no invalid IPs, break the loop.
+        if not invalid_ip_addresses:  # If there are no invalid IPs, break the loop.
             break
 
         # If there are invalid IPs, notify the user and continue the loop.
-        print("Invalid IPs or subnets: " + ", ".join(invalid_ips))
+        print("Invalid IPs or subnets: " + ", ".join(invalid_ip_addresses))
 
     return networks
 
-
 def exclude_networks(allowed_networks, disallowed_networks):
     remaining_networks = set(allowed_networks)
+    
     for disallowed in disallowed_networks:
-        for allowed in allowed_networks:
-            if allowed.overlaps(disallowed):
-                remaining_networks.difference_update([allowed])
-                remaining_networks.update(allowed.address_exclude(disallowed))
+        new_remaining_networks = set()
+        
+        for allowed in remaining_networks:
+            if allowed.version == disallowed.version:  # Ensure we're comparing the same IP versions
+                if disallowed.subnet_of(allowed):
+                    # If the disallowed network is a subnet of the allowed network, exclude it
+                    new_remaining_networks.update(allowed.address_exclude(disallowed))
+                elif allowed.overlaps(disallowed):
+                    # If there's an overlap but no network is a subnet of the other, we need to handle this case separately.
+                    # For the scope of this fix, we're not handling partial overlaps.
+                    continue
+                else:
+                    # If there's no overlap, keep the allowed network as it is.
+                    new_remaining_networks.add(allowed)
+            else:
+                # If the IP versions don't match, keep the allowed network as it is.
+                new_remaining_networks.add(allowed)
+
+        # Update the remaining networks after processing each disallowed network
+        remaining_networks = new_remaining_networks
+
     return remaining_networks
 
+
 def sort_networks(networks):
+    """Sort IP networks with all IPv4 first, then IPv6, each from lowest to highest."""
     ipv4 = []
     ipv6 = []
     for net in networks:
@@ -53,48 +72,67 @@ def sort_networks(networks):
             ipv4.append(net)
         else:
             ipv6.append(net)
-    return sorted(ipv4) + sorted(ipv6)
+    # Sort each list individually
+    ipv4_sorted = sorted(ipv4, key=lambda ip: ip.network_address)
+    ipv6_sorted = sorted(ipv6, key=lambda ip: ip.network_address)
+
+    # Combine the lists with all IPv4 addresses first, then IPv6
+    return ipv4_sorted + ipv6_sorted
 
 def main():
-    # Check if there are command-line arguments
-    if len(sys.argv) > 3:
-        print("Error: Too many arguments. Only two arguments are allowed.")
-        print("WireGuard_Excluded_IPs.py <AllowedIPs> <DisallowedIPs>")
-        sys.exit(1)  # Exit with an error code
+    allowed_input = ''
+    disallowed_input = ''
+    allowed_networks = []
+    disallowed_networks = []
 
-    elif len(sys.argv) == 3:
-        allowed_input = sys.argv[1]  # The first argument after the script name
-        disallowed_input = sys.argv[2]  # The second argument after the script name
+    # Validate command line arguments
+    if len(sys.argv) == 3:
+        allowed_input = sys.argv[1]
+        disallowed_input = sys.argv[2]
+    elif len(sys.argv) == 2:
+        allowed_input = sys.argv[1]
+    elif len(sys.argv) > 3:
+        print("Error: Too many arguments provided.")
+        # Reset inputs to fall back to interactive mode
+        allowed_input = ''
+        disallowed_input = ''
 
-        # Directly parse the command-line arguments
-        allowed_networks, invalid_allowed_ips = parse_ip_networks(allowed_input)
-        disallowed_networks, invalid_disallowed_ips = parse_ip_networks(disallowed_input)
+    # Validate and parse command line arguments or get user input if arguments are invalid or not provided.
+    if allowed_input:
+        allowed_networks, invalid_allowed = parse_ip_networks(allowed_input)
+        if invalid_allowed:
+            print("Invalid Allowed IPs: " + ", ".join(invalid_allowed))
+            allowed_networks = []  # Reset to empty to trigger interactive mode for allowed IPs
 
-        # If there are invalid IPs, print them and exit
-        if invalid_allowed_ips or invalid_disallowed_ips:
-            if invalid_allowed_ips:
-                print("Invalid Allowed IPs: " + ", ".join(invalid_allowed_ips))
-            if invalid_disallowed_ips:
-                print("Invalid Disallowed IPs: " + ", ".join(invalid_disallowed_ips))
-            sys.exit(1)
+    if disallowed_input:  # This ensures it won't run if there's no disallowed_input
+        disallowed_networks, invalid_disallowed = parse_ip_networks(disallowed_input)
+        if invalid_disallowed:
+            print("Invalid Disallowed IPs: " + ", ".join(invalid_disallowed))
+            disallowed_networks = []  # Reset to empty to trigger interactive mode for disallowed IPs
 
-    else:
-        # If no arguments, proceed with interactive mode
-        print("You can optionally provide command-line arguments as follows:")
-        print("WireGuard_Excluded_IPs.py <AllowedIPs> <DisallowedIPs>")
-        print("Example: WireGuard_Excluded_IPs.py '0.0.0.0/0' '10.0.0.0/8,127.0.0.0/8,172.16.0.0/12,192.168.0.0/16'")
-        print("If no arguments are provided, you will be prompted for input.\n")
-
+    # If inputs were invalid or not provided, switch to interactive mode.
+    if not allowed_networks:
         allowed_networks = get_input_and_parse("Enter the Allowed IPs, comma separated (e.g., 0.0.0.0/0):\n")
+
+    if not disallowed_networks:
         disallowed_networks = get_input_and_parse("Enter the Disallowed IPs, comma separated (e.g., 10.0.0.0/8,127.0.0.0/8,172.16.0.0/12,192.168.0.0/16):\n")
 
-    if allowed_networks is None or disallowed_networks is None:
-        return
+    # Process the IP networks.
+    excluded_allowed_networks = exclude_networks(allowed_networks, disallowed_networks)
 
-    remaining_networks = exclude_networks(allowed_networks, disallowed_networks)
-    sorted_networks = sort_networks(remaining_networks)
+    # Sort the networks with IPv4 first, then IPv6.
+    sorted_networks = sort_networks(excluded_allowed_networks)
 
-    print("AllowedIPs =", ', '.join(str(net) for net in sorted_networks))
+    if not sorted_networks:
+        print("Error: No IPs are allowed based on the provided input.")
+        sys.exit(1)
+
+    # Print the initial inputs and final result.
+    print("\nSummary:")
+    print("AllowedIPs    = " + ', '.join(map(str, allowed_networks)))
+    print("DisallowedIPs = " + ', '.join(map(str, disallowed_networks)))
+    print("=======================")
+    print("AllowedIPs    = " + ', '.join(map(str, sorted_networks)))
 
 if __name__ == "__main__":
     main()
