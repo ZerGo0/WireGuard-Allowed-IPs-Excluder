ccbe3d9f3ebecdf9b470e5429979684dce8592d3
p1r473 <subwayjared@gmail.com>
2023-10-18 15:20:41 -0400

    Quality of life improvements (#2)
    * V2.0
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update README.md
    
    * Update README.md
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update README.md
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update WireGuard_Excluded_IPs.py
    
    * Update WireGuard_Excluded_IPs.py
    
    * Applied formatting
    
    * Added additional logging
    
    * Added tests
    
    ---------
    
    Co-authored-by: ZerGo0 <18653821+ZerGo0@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,4 +1,94 @@
 # WireGuard-Allowed-IPs-Excluder
-Allows you to create a IPv4 List with single IPs excluded. (For local device access for example)
+Use the following form when you need to calculate complex AllowedIPs settings for a WireGuard peer, by subtracting the “disallowed” IP address blocks from the “allowed” IP address blocks.
 
-Syntax: WireGuard_Excluded_IPs.py 8.8.8.8 (1.1.1.1)
+Syntax: WireGuard-Allowed-IPs-Excluders.py [AllowedIPs] [DisallowedIPs]
+
+For example, say you wanted to route everything in the `10.0.0.0/8` block of IP addresses through a WireGuard peer — except you also wanted to exclude the smaller `10.0.1.0/24` block from it. In the `Allowed IPs` field, input `10.0.0.0/8`; in the `Disallowed IPs` field, input `10.0.1.0/24`; and click the Calculate button. This is the result you’ll get (which is what you enter into your WireGuard config):
+
+```
+AllowedIPs = 10.0.0.0/24, 10.0.2.0/23, 10.0.4.0/22, 10.0.8.0/21, 10.0.16.0/20, 10.0.32.0/19, 10.0.64.0/18, 10.0.128.0/17, 10.1.0.0/16, 10.2.0.0/15, 10.4.0.0/14, 10.8.0.0/13, 10.16.0.0/12, 10.32.0.0/11, 10.64.0.0/10, 10.128.0.0/9
+```
+
+Or as another example, say you wanted to route everything but the standard private, local, or link-scoped IP address blocks through a WireGuard peer. In the `Allowed IPs` field, input `0.0.0.0/0`, `::/0`; in the `Disallowed IPs` field, input `0.0.0.0/8`, `10.0.0.0/8`, `127.0.0.0/8`, `169.254.0.0/16`, `172.16.0.0/12`, `192.168.0.0/16`, `240.0.0.0/4`, `fc00::/7`, `fe80::/10`; and click the Calculate button. This is the result you’ll get:
+
+```
+AllowedIPs = 1.0.0.0/8, 2.0.0.0/7, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/3, 96.0.0.0/4, 112.0.0.0/5, 120.0.0.0/6, 124.0.0.0/7, 126.0.0.0/8, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/8, 169.0.0.0/9, 169.128.0.0/10, 169.192.0.0/11, 169.224.0.0/12, 169.240.0.0/13, 169.248.0.0/14, 169.252.0.0/15, 169.255.0.0/16, 170.0.0.0/7, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4, 224.0.0.0/4, `::/1`, `8000::/2`, `c000::/3`, `e000::/4`, `f000::/5`, `f800::/6`, `fe00::/9`, `fec0::/10`, `ff00::/8`
+```
+
+BACKGROUND
+
+You use the `AllowedIPs` setting of WireGuard to configure which blocks of IP addresses should be routed through which remote WireGuard peers. If you want to access everything through a peer, configure its `AllowedIPs` setting to the following:
+
+```
+AllowedIPs = 0.0.0.0/0, ::/0
+```
+
+This indicates to WireGuard that all IPv4 addresses (`0.0.0.0/0`) and all IPv6 addresses (`::/0`) should be routed through the peer. Note that you can specify multiple blocks of addresses on the same line, separated by commas, like above; or you can specify them individually on separate lines, like below:
+
+```
+AllowedIPs = 0.0.0.0/0
+AllowedIPs = ::/0
+```
+
+If you want to access just a single block of IP addresses through a WireGuard peer, like say a block of IP addresses at a remote site that range from `192.168.100.0` to `192.168.100.255`, you’d set the `AllowedIPs` for it to the following:
+
+```
+AllowedIPs = 192.168.100.0/24
+```
+
+This indicates to WireGuard that only the `192.168.100.0/24` block of IP addresses should be routed through the peer. If you want to access multiple blocks of IP addresses through a WireGuard peer, you’d set the `AllowedIPs` for it to the following:
+
+```
+AllowedIPs = 192.168.100.0/24, 192.168.101.0/24
+```
+
+This indicates to WireGuard that both the `192.168.100.0/24` and `192.168.101.0/24` blocks of IP addresses should be routed through the peer. If you want to access everything but a certain block of IP addresses through a WireGuard peer, you’d set the `AllowedIPs` for it to the following:
+
+```
+AllowedIPs = 0.0.0.0/0, ::/0
+```
+
+And then you’d add a route on your local system to access that block of IP addresses through your regular Internet connection, like so:
+
+```
+`ip route add 192.168.100.0/24 via 192.168.1.1 dev eth0`
+`ip route del 192.168.100.0/24 via 192.168.1.1 dev eth0`
+```
+
+You’d replace `192.168.1.1` with the IP address of your regular Internet gateway, and `eth0` with the name of your regular Internet network interface.
+
+But what if you want to access everything but certain blocks of IP addresses through a WireGuard peer? This is where it gets complicated, because you can’t just add a route on your local system for each block of IP addresses you don’t want to access through the peer, because your system will still send packets to those IP addresses to the peer if its `AllowedIPs` is set to the following:
+
+```
+AllowedIPs = 0.0.0.0/0, ::/0
+```
+
+Instead, you need to set the `AllowedIPs` for the peer to everything but those blocks of IP addresses. And this is where our `AllowedIPs` Calculator comes in: It lets you easily calculate the `AllowedIPs` setting for a WireGuard peer by subtracting the “disallowed” IP address blocks from the “allowed” IP address blocks.
+
+For example, say you wanted to access everything but the `10.0.0.0/8` block of IP addresses through a WireGuard peer. You’d input the following into the Calculator:
+
+`Allowed IPs`: `0.0.0.0/0`, `::/0`
+`Disallowed IPs`: `10.0.0.0/8`
+
+And this is the result you’d get:
+
+```
+AllowedIPs = 0.0.0.0/5, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/1, ::/0
+```
+
+Or as another example, say you wanted to access everything but the standard private, local, or link-scoped IP address blocks through a WireGuard peer. You’d input the following into the Calculator:
+
+`Allowed IPs`: `0.0.0.0/0`, `::/0`
+`Disallowed IPs`: `0.0.0.0/8`, `10.0.0.0/8`, `127.0.0.0/8`, `169.254.0.0/16`, `172.16.0.0/12`, `192.168.0.0/16`, `240.0.0.0/4`, `fc00::/7`, `fe80::/10`
+
+And this is the result you’d get:
+
+```
+AllowedIPs = 1.0.0.0/8, 2.0.0.0/7, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/3, 96.0.0.0/4, 112.0.0.0/5, 120.0.0.0/6, 124.0.0.0/7, 126.0.0.0/8, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/8, 169.0.0.0/9, 169.128.0.0/10, 169.192.0.0/11, 169.224.0.0/12, 169.240.0.0/13, 169.248.0.0/14, 169.252.0.0/15, 169.255.0.0/16, 170.0.0.0/7, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4, 224.0.0.0/4, `::/1`, `8000::/2`, `c000::/3`, `e000::/4`, `f000::/5`, `f800::/6`, `fe00::/9`, `fec0::/10`, `ff00::/8`
+```
+
+This indicates to WireGuard that everything but the specified blocks of IP addresses should be routed through the peer.
+
+The Calculator works by first expanding the “allowed” and “disallowed” IP address blocks into their individual IP addresses, and then subtracting the “disallowed” IP addresses from the “allowed” IP addresses. It then combines the resulting IP addresses back into blocks, and sorts and merges those blocks to produce the final `AllowedIPs` setting.
+
+The Calculator supports both IPv4 and IPv6 addresses, and you can input the IP address blocks into it in CIDR notation (like `192.168.100.0/24` or `::/0`) or with a subnet mask (like `192.168.100.0/255.255.255.0` or `::/128`). It also supports the special IP address blocks `0.0.0.0/0` and `::/0`, which represent all IPv4 addresses and all IPv6 addresses, respectively.
--- WireGuard_Excluded_IPs.py
+++ WireGuard_Excluded_IPs.py
@@ -1,44 +1,192 @@
 import ipaddress
-import re
 import sys
 
-arguments = sys.argv[1:]
-if len(arguments) is 0:
-    print("Syntax: WireGuard_Excluded_IPs.py 8.8.8.8 (1.1.1.1)")
-    exit()
 
-for arg in sys.argv[1:]:
-    if re.search("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", arg) is None:
-        print("Invalid IP Address: {}".format(arg))
-        exit()
-        break
-    pass
+def parse_ip_networks(ip_list_str):
+    ip_list = ip_list_str.split(",")
+    networks = []
+    invalid_ip_addresses = []  # List to store invalid IPs.
 
-mainBlockRange = ipaddress.ip_network('0.0.0.0/0')
-listUpdatedMainBlockRange = list(mainBlockRange.address_exclude(
-    ipaddress.ip_network('{}/32'.format(sys.argv[1]))))
-listUpdatedMainBlockRange.reverse()
-listMainBlockRange = listUpdatedMainBlockRange
+    for ip in ip_list:
+        ip = ip.strip()
+        try:
+            if "/" in ip:
+                networks.append(ipaddress.ip_network(ip, strict=False))
+            else:
+                ip_obj = ipaddress.ip_address(ip)
+                if ip_obj.version == 4:
+                    networks.append(ipaddress.ip_network(f"{ip}/32", strict=False))
+                else:
+                    networks.append(ipaddress.ip_network(f"{ip}/128", strict=False))
+        except ValueError:
+            invalid_ip_addresses.append(ip)  # Add invalid IP to the list.
 
-for arg in sys.argv[1:]:
-    tempIP = ipaddress.IPv4Address(arg)
-    print("tempIP: {}".format(tempIP))
-    i = 0
+    return networks, invalid_ip_addresses  # Return both valid networks and invalid IPs.
 
-    for tempBlockRange in listUpdatedMainBlockRange:
-        print("tempBlockRange: {}".format(tempBlockRange))
-        if tempIP in tempBlockRange:
-            print("tempIP: {} | tempBlockRange: {}".format(
-                tempIP, tempBlockRange))
-            listMainBlockRange.remove(tempBlockRange)
-            tempRange = ipaddress.ip_network(tempBlockRange)
-            listMainBlockRange.insert(i, list(
-                tempRange.address_exclude(ipaddress.ip_network('{}/32'.format(arg)))))
-        i += 1
 
-    listUpdatedMainBlockRange = listMainBlockRange
+def get_input_and_parse(prompt):
+    while True:  # Keep looping until we break out.
+        user_input = input(prompt)
+        networks, invalid_ip_addresses = parse_ip_networks(
+            user_input
+        )  # Get both valid networks and invalid IPs.
 
-listMainBlockRange.reverse()
-print("\n\n\n")
-print("AllowedIPs = {}, ::/1, 8000::/1".format(str(list(listMainBlockRange)).replace("[", "").replace(
-    "IPv4Network('", "").replace("')", "").replace("]", "")))
+        if not invalid_ip_addresses:  # If there are no invalid IPs, break the loop.
+            break
+
+        # If there are invalid IPs, notify the user and continue the loop.
+        print("Invalid IPs or subnets: " + ", ".join(invalid_ip_addresses))
+        print("Please try again. Ctrl+C to exit.")
+
+    return networks
+
+
+def exclude_networks(allowed_networks, disallowed_networks):
+    remaining_networks = set(allowed_networks)
+
+    for disallowed in disallowed_networks:
+        new_remaining_networks = set()
+
+        for allowed in remaining_networks:
+            if allowed.version == disallowed.version:
+                if disallowed.subnet_of(allowed):
+                    # If the disallowed network is a subnet of the allowed network, exclude it
+                    new_remaining_networks.update(allowed.address_exclude(disallowed))
+                elif allowed.overlaps(disallowed):
+                    # Handle partial overlap
+                    new_remaining_networks.update(
+                        handle_partial_overlap(allowed, disallowed)
+                    )
+                else:
+                    # If there's no overlap, keep the allowed network as it is.
+                    new_remaining_networks.add(allowed)
+            else:
+                # If the IP versions don't match, keep the allowed network as it is.
+                new_remaining_networks.add(allowed)
+
+        # Update the remaining networks after processing each disallowed network
+        remaining_networks = new_remaining_networks
+
+    return remaining_networks
+
+
+def handle_partial_overlap(allowed, disallowed):
+    # This function will handle the case of a partial overlap and return the non-overlapping portions of the allowed network.
+    non_overlapping_networks = []
+
+    # Calculate the IPs for the allowed and disallowed networks
+    allowed_ips = list(allowed.hosts())
+    disallowed_ips = set(disallowed.hosts())  # Use a set for faster lookup
+
+    # Filter out the disallowed IPs
+    allowed_ips = [ip for ip in allowed_ips if ip not in disallowed_ips]
+
+    if not allowed_ips:
+        # If no IPs are left, there's nothing to add
+        return non_overlapping_networks
+
+    # Create new network(s) from the remaining IPs.
+    # This is a simplistic way and works on individual IPs, not ranges.
+    # You might need a more efficient way to handle ranges of IPs, especially for large networks.
+    for ip in allowed_ips:
+        if ip.version == 4:
+            non_overlapping_networks.append(
+                ipaddress.ip_network(f"{ip}/32", strict=False)
+            )
+        else:
+            non_overlapping_networks.append(
+                ipaddress.ip_network(f"{ip}/128", strict=False)
+            )
+
+    return non_overlapping_networks
+
+
+def sort_networks(networks):
+    """Sort IP networks with all IPv4 first, then IPv6, each from lowest to highest."""
+    ipv4 = []
+    ipv6 = []
+    for net in networks:
+        if net.version == 4:
+            ipv4.append(net)
+        else:
+            ipv6.append(net)
+    # Sort each list individually
+    ipv4_sorted = sorted(ipv4, key=lambda ip: ip.network_address)
+    ipv6_sorted = sorted(ipv6, key=lambda ip: ip.network_address)
+
+    # Combine the lists with all IPv4 addresses first, then IPv6
+    return ipv4_sorted + ipv6_sorted
+
+
+def main(unittest=False):
+    allowed_input = ""
+    disallowed_input = ""
+    allowed_networks = []
+    disallowed_networks = []
+
+    # Validate command line arguments
+    if len(sys.argv) == 3:
+        allowed_input = sys.argv[1]
+        disallowed_input = sys.argv[2]
+    elif len(sys.argv) == 2:
+        disallowed_input = sys.argv[1]
+    else:
+        print("Wrong number of arguments provided, falling back to interactive mode.")
+        # Reset inputs to fall back to interactive mode
+        allowed_input = ""
+        disallowed_input = ""
+
+    # Validate and parse command line arguments or get user input if arguments are invalid or not provided.
+    if allowed_input:
+        allowed_networks, invalid_allowed = parse_ip_networks(allowed_input)
+        if invalid_allowed:
+            print("Invalid Allowed IPs: " + ", ".join(invalid_allowed))
+            allowed_networks = (
+                []
+            )  # Reset to empty to trigger interactive mode for allowed IPs
+
+    if disallowed_input:  # This ensures it won't run if there's no disallowed_input
+        disallowed_networks, invalid_disallowed = parse_ip_networks(disallowed_input)
+        if invalid_disallowed:
+            print("Invalid Disallowed IPs: " + ", ".join(invalid_disallowed))
+            disallowed_networks = (
+                []
+            )  # Reset to empty to trigger interactive mode for disallowed IPs
+
+    # If inputs were invalid or not provided, switch to interactive mode.
+    if not allowed_networks and not len(sys.argv) == 2:
+        allowed_networks = get_input_and_parse(
+            "Enter the Allowed IPs, comma separated (e.g., 0.0.0.0/0):\n"
+        )
+
+    if not disallowed_networks:
+        disallowed_networks = get_input_and_parse(
+            "Enter the Disallowed IPs, comma separated (e.g., 10.0.0.0/8,127.0.0.0/8,172.16.0.0/12,192.168.0.0/16):\n"
+        )
+
+    # Process the IP networks.
+    excluded_allowed_networks = exclude_networks(allowed_networks, disallowed_networks)
+
+    # Sort the networks with IPv4 first, then IPv6.
+    sorted_networks = sort_networks(excluded_allowed_networks)
+
+    if not sorted_networks:
+        print("Error: No IPs are allowed based on the provided input.")
+        sys.exit(1)
+
+    # Print the initial inputs and final result.
+    print("Input:")
+    print("AllowedIPs = " + ", ".join(map(str, allowed_networks)))
+    print("DisallowedIPs = " + ", ".join(map(str, disallowed_networks)))
+    print()
+    print("=======================")
+    print()
+    print("Output:")
+    print("AllowedIPs = " + ", ".join(map(str, sorted_networks)))
+
+    if unittest:
+        return sorted_networks
+
+
+if __name__ == "__main__":
+    main()
